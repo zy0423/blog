2e5ba7bb7028705dca91bbcbb6679627e3bf71f0
zengyi0423 <zengyi0423@163.com>
2016-02-17 16:27:33 +0800

    mongdb

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+var db = require('../db/db');
+
+var kittySchema = mongoose.Schema({
+  name: String
+})
+
+kittySchema.methods.speak = function () {
+  var greeting = this.name
+      ? "Meow name is " + this.name
+      : "I don't have a name"
+  console.log(greeting);
+}
+
+var Kitten = mongoose.model('Kitten', kittySchema)
+
+var fluffy = new Kitten({name: 'fluffy'});
+// "Meow name is fluffy"
+//fluffy.speak();
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  /*  fluffy.save(function (err, fluffy) {
+   if (err) return console.error(err);
+   fluffy.speak();
+   });*/
+
+  Kitten.find({name: 'fluffy'}, function (err, kittens) {
+    if (err) return console.error(err);
+    console.log(kittens)
+  })
+
+  res.render('index', {title: 'Kuye'});
 });
 
 module.exports = router;
